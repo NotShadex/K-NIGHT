4110efb1b84a57fa5babef883ea1e19b489bce48
NotShadex <maks.brolih@gmail.com>
2025-04-06 15:16:26 +0200

    New sounds and new booleans. Added the VARIABLE background that reads .csv and creates the background similar to OBJECTS. Modified the start position. Added the font init. Fixed the imports a bit.

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import pygame
 from terrain import *
-from visual import *
+from effects import *
+from sprite_cutter import *
+import time
+from random import randint
+from os.path import isfile, join
+import csv
 
 # GAME SETUP & CONST VALUES
 GAME_TITLE = "K-NIGHT"
@@ -14,6 +20,9 @@ pygame.display.set_caption(GAME_TITLE)
 # INITIALIZE MIXER
 pygame.mixer.init()
 pygame.mixer.set_num_channels(16)
+
+# INITIALIZE FONTS
+pygame.font.init()
 
 # HEARTS
 heart_full = pygame.image.load("assets/GUI/full_heart.png").convert_alpha()
@@ -32,24 +41,27 @@ SCROLL_AREA_HEIGHT = 250
 # BLOCK SIZE
 BLOCK_SIZE = 48
 
-# BACKGROUND
-BACKGROUND_LAYER_0 = "Background_0.png"
-BACKGROUND_LAYER_1 = "Background_1.png"
-
 # TILEMAP
-OBJECTS = get_map(read_csv(), BLOCK_SIZE)
+OBJECTS = get_map(read_csv("new_Tiles.csv"), BLOCK_SIZE)
+
+# BACKGROUND
+BACKGROUND = get_map(read_csv("new_Background.csv"), BLOCK_SIZE)
 
 # PLAYER INITIAL POSITION
 PLAYER_START_X = 100
-PLAYER_START_Y = 1000
+PLAYER_START_Y = 700
 PLAYER_WIDTH = 120
 PLAYER_HEIGHT = 60
 
 # BOSS INITIAL POSITION
-BOSS_START_X = 1500
-BOSS_START_Y = 81
+BOSS_START_X = 7000
+BOSS_START_Y = 560
 BOSS_WIDTH = 288
 BOSS_HEIGHT = 160
+
+# PLAYER IN ARENA
+IN_ARENA = False
+ARENA_POS = 2500
 
 # SOUND EFFECTS
 path = join("assets", "Sound Effects", "")
@@ -64,12 +76,16 @@ CAST = pygame.mixer.Sound(path+"cast.wav")
 EXPLOSION = pygame.mixer.Sound(path+"explosion.wav")
 HURT = pygame.mixer.Sound(path+"hurt.wav")
 COLUMN = pygame.mixer.Sound(path+"column.wav")
+BOSS_HURT = pygame.mixer.Sound(path+"boss_hurt.wav")
+DEATH = pygame.mixer.Sound(path+"death.wav")
+PLAYED = False
+SHRIEK_PLAYED = False
 
 # VOLUME SET
 SHRIEK.set_volume(0.3)
 HURT.set_volume(0.3)
+BOSS_HURT.set_volume(0.8)
 BOSS_ATK.set_volume(0.7)
 EXPLOSION.set_volume(0.5)
 CAST.set_volume(0.2)
 COLUMN.set_volume(0.2)
-
